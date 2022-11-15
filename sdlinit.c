a7ea0c2b7004e24e852376c1741f5e4a77468f2d
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-11-15 12:09:43 +0300

    init

---
+++ sdlinit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "SDL2/SDL.h"
+
+int main(int argc, char *argv[])
+{
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+  {
+    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+    return 1;
+  }
+  else
+  {
+    printf("sdl initialized.");
+  }
+
+  /* ... */
+
+  SDL_Quit();
+
+  return 0;
+}
